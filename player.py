5142a411d935e4b61c7f5aea88943c3886ab51b7
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-03-03 17:17:33 -0300

    first

--- player.py
+++ player.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("brown")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE + 50
+        self.goto(self.xcor(), new_y)
+
+    def finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            self.goto(STARTING_POSITION)
